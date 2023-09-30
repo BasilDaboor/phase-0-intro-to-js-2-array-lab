85b84045e00a00f9aceab6047e133d5debc92ffa
basil daboor <basildaboor12@gmail.com>
2023-09-30 22:10:03 +0300

    Mission completed

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+
+let  cats =["Milo", "Otis", "Garfield"];
+
+  function destructivelyAppendCat(cat){
+cats.push(cat)
+}
+
+function destructivelyPrependCat(cat){
+
+  cats.unshift(cat);
+}
+
+  function destructivelyRemoveLastCat(cat){
+
+cats.pop(cat);
+
+}
+function destructivelyRemoveFirstCat(cat) {
+  cats.shift(cat);
+}
+
+function appendCat(cat) {
+  let newCatsArray = [...cats, cat];
+  return newCatsArray;
+}
+function prependCat(cat) {
+  let newCatsArray = [cat,...cats];
+  return newCatsArray;
+}
+
+function removeLastCat() {
+  let newCatsArray = [...cats];
+  newCatsArray.pop();
+  return newCatsArray;
+}
+
+// After I finished the tasks, I looked for other ways to solve it and found this
+// function removeLastCat() {
+//   const newCatsArray = cats.slice(0, -1);
+//   return newCatsArray;
+// }
+
+function removeFirstCat() {
+  const newCatsArray = cats.slice(1);
+  return newCatsArray;
+}
